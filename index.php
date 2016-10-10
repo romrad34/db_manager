8c1abba6f42662e56137bb3b76edf34e1a366d80
romrad34 <romain.itofo@gmail.com>
2016-10-10 23:39:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 ini_set('display_errors', 1);
 
 use \NSclasses\Autoloader;
-//use \NSclasses\Config;
-//use \NSclasses\DisplayTables;
+
 require ('classes/Autoloader.php');
 Autoloader::register();
 require ('vue/vue_connexion.html');
